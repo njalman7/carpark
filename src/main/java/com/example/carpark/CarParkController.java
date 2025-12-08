9ad12e23769e29bcc2b1a4063eb004005c527b9d
Kevin Butgereit <kevin.butgereit@live.de>
2025-12-08 11:38:54 +0100

    FXML initialisiert

--- src/main/java/com/example/carpark/CarParkController.java
+++ src/main/java/com/example/carpark/CarParkController.java
@@ -1,14 +1,25 @@
 package com.example.carpark;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 public class CarParkController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label anzeige;
+
+    @FXML
+    private ComboBox<?> levelSelector;
+
+    @FXML
+    private Pane parkspace;
+
+    @FXML
+    void onShow(ActionEvent event) {
+
     }
+
 }
